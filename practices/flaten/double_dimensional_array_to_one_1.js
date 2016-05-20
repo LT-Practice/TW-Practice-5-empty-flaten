37f30e30d3976e2b2ad3ae0933580547d25a7b40
Leonard LiuTao <364125168@qq.com>
2016-05-20 22:45:33 +0800

    2

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -3,30 +3,25 @@
 function double_to_one(collection) {
 
   //在这里写入代码
-  var arr = [];
-  var n = collection.length;
-  for (var i = 0; i < n; i++) {
-    if (collection[i].length >= 1) {
-
-      var m = collection[i].length;
-
-      for (var j = 0; j < m; j++) {
-
-        arr.push(collection[i][j]);
-
-      }
-
-    }
-
-      else {
-
-        arr.push(collection[i]);
-
-      }
-
+  var Arr = [];
+collection.forEach(function (element) {
+  if (element[0]) {
+    getElement(element, Arr);
   }
+  else {
+    Arr.push(element);
+  }
+});
 
-return arr;
+return Arr;
+}
+
+function getElement(elementArr, Arr) {
+elementArr.forEach(function (item) {
+  Arr.push(item);
+})
+
+
 
 }
 module.exports = double_to_one;
